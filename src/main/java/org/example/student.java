562ae9a3552370113419acfa1d75dc27f095b4cb
84206 <842063349@qq.com>
2022-03-29 10:26:24 +0800

    123456

--- src/main/java/org/example/student.java
+++ src/main/java/org/example/student.java
@@ -3,4 +3,29 @@ package org.example;
 public class student {
     String id ;
     String name;
+
+    @Override
+    public String
+    toString() {
+        return "student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
